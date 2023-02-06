97ef5a63054256c4e91969b57e9a67998d609d1e
Dinesh-kds97 <124577045+Dinesh-kds97@users.noreply.github.com>
2023-02-06 16:44:54 +0530

    Add files via upload

---
+++ ClassRoom2EvenNumbers.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class ClassRoom2EvenNumbers {
+
+	public static void main(String[] args) {
+		
+		   for (int i = 0; i <= 50; i++) {
+			
+			   if (i%2==0) {
+				   System.out.println(i + " is a even number");
+			   }
+				   
+		}
+
+	}
+
+}
